4206a114d78e9ed49656d463a4aefc44be18d383
ATUL KUMAR YADAV <55950948+atul2oct@users.noreply.github.com>
2023-05-13 23:11:42 +0530

    add layout partial and home page

--- index.js
+++ index.js
@@ -5,8 +5,26 @@ require('dotenv').config();
 const port = process.env.PORT || 8000;
 
 const db = require('./config/mongoose');
+db();
 
-app.use('/', require('./routes'));
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(express.urlencoded());
+app.use(express.static('assets'));
+app.use(expressLayouts);
+
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+//use express router
+app.use('/',require('./routes/index'));
+
+//set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
 app.listen(port, function (error) {
   if (error) {
     console.log(`Error in running the server: ${error}`);
